7dfcb204d1388b8a4f3e5d0828b36d828cda0b8e
Jitendra Prajapati <prajapatijitendra2848@gmail.com>
2026-06-25 15:48:16 +0530

    Actions in gitub composite,reusable,js/ts and docker

---
+++ .github/actions/docker-action/entrypoint.sh
@@ -0,0 +1,8 @@
+
+echo "Docker Action Started"
+
+USERNAME=$1
+
+echo "Hello $USERNAME from Docker Action"
+
+bun run index.ts "$USERNAME"
---
+++ .github/actions/js-action/src/index.ts
@@ -0,0 +1,8 @@
+import * as core from '@actions/core';
+
+try {
+  const username = core.getInput('username');
+  core.info(`Hello : ${username || "Name not found!"}`);
+} catch (error :any) {
+  core.setFailed(error.message);
+}
